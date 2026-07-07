005dd6a00c1087a8cbfba935abb8140fac1ae78c
LetTTGACO <1225751694@qq.com>
2026-07-07 19:28:07 +0800

    feat: 升级 elog

--- elog.config.1x.ts
+++ elog.config.1x.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@elog/cli";
+import { defineConfig } from "@elog/core";
 import fromNotion from "@elog/plugin-from-notion";
 import toLocal from "@elog/plugin-to-local";
 import imageR2 from "@elog/plugin-transform-image-r2";
